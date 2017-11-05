9f23430857f50d9ca9c76d020f0199233769b7cb
Steven Frieson <sfrieson@gmail.com>
2017-11-05 16:44:49 -0500

    Connects queries to db

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,18 +1,43 @@
+import db from '../db';
+
+const defaultMaxResults = 10;
+
 export default {
   Query: {
-    records: function (_, {count}) {
-      return [{food: 'fake peanutbutter'}];
+    records: function (_, {count = defaultMaxResults}) {
+      return db.query({
+        text: 'SELECT * FROM records LIMIT $1;',
+        values: [count]
+      }).then(res => res.rows);
     },
-    reports: function (_, {count}) {
-      return [{symptom: 'headache'}];
+    reports: function (_, {count = defaultMaxResults}) {
+      return db.query({
+        text: 'SELECT * FROM reports LIMIT $1;',
+        values: [count]
+      }).then(res => res.rows);
     }
   },
   Mutation: {
     record: function (_, {data}) {
-      return !!data;
+      const keys = Object.keys(data);
+
+      return db.query({
+        text: `INSERT INTO
+          records (${keys.join(', ')})
+          VALUES (${keys.map((_, i) => `$${i + 1}`).join(', ')});`,
+        values: keys.map(key => data[key])
+      }).then(() => true)
+      .catch(() => false);
     },
     report: function (_, {data}) {
-      return !!data;
+      const keys = Object.keys(data);
+
+      return db.query({
+        text: `INSERT INTO
+          reports (${keys.join(', ')})
+          VALUES (${keys.map((_, i) => `$${i + 1}`).join(', ')});`,
+        values: keys.map(key => data[key])
+      });
     }
   }
 };
